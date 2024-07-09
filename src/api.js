1a4f0e1251c479ab57a3ef21f8d1d8a37a1c476f
Aleksey Averianov <Flex_d1@mail.ru>
2024-07-09 09:27:42 +0300

    Скорректирован axios intersceptors, добавлены исключения для запросов авторизации/регистрации

--- src/api.js
+++ src/api.js
@@ -2,7 +2,9 @@ import { useAuthStore } from '@/stores/auth.js'
 import axios from "axios";
 
 axios.interceptors.request.use((config) => {
-  const authStore = useAuthStore(); // получаем данные из стора
-  config.headers.authorization = `Bearer ${authStore.userInfo.token}`; // добавляем в header токен
+  if (config.url !== 'http://5.35.86.160:3000/users/' || config.url !== 'http://5.35.86.160:3000/users/auth/') {
+    const authStore = useAuthStore(); // получаем данные из стора
+    config.headers.authorization = `Bearer ${authStore.userInfo.token}`; // добавляем в header токен
+  }
   return config; //возвращаем config
 })
